5f318a2cba82093d412c8f6d8443ea807211732b
sophia <sophia.zhu10@gmail.com>
2018-08-23 22:23:58 +1000

    challenge1b.rb

---
+++ challenge1.rb
@@ -0,0 +1,9 @@
+puts 'Hello, how old are you?'
+age= gets.to_i  
+    if age >18
+        puts 'welcome to the casino'
+    elsif age == 16
+        puts 'sweet sixteen'
+    else age <=18
+        puts 'sorry,you are too young'
+end
---
+++ challenge1b.rb
@@ -0,0 +1,11 @@
+myname= 'sophia'
+puts "my name is #{myname} zhu"
+puts "my name is "+myname+" zhu" 
+mysiblings= 2
+puts"total siblings:#{mysiblings}"
+puts"new total siblings:#{mysiblings+1.to_i}"   #using interpolation
+
+
+mysiblings= 2+1
+puts "new total siblings:"+mysiblings.to_s
+
