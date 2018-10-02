ac6443c642e534320e4cb4ffa1b220831491ffb9
sophia <sophia.zhu10@gmail.com>
2018-10-02 20:51:44 +1000

    mvc

--- customer/test/customer_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require_relative '../customer'
-class CustomerTest < Test::Unit::TestCase
-    def test_first_name
-        customer = Customer.new('John','Smith')
-        assert_equal(customer.first_name,'John')
-    end
-end
-
